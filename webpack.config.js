24bc39b50b8deeaa0a1930e973c2d9a632a3697e
Derrick Marshall <marshallderrickdnm@gmail.com>
2021-02-15 14:35:56 -0800

    service is init

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './client/components/index.jsx',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, './client/dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?jsx$/,
+        exclude: /(node_modules | bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
+      }
+    ]
   }
 }
